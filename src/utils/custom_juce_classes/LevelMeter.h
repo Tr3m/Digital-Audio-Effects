152092e40f45aa4c022e5c465704c4c3bcb156b4
tr3m <tr3m@archbox2>
2023-12-21 12:36:21 +0200

    Added clipping indicator to level meters

--- src/utils/custom_juce_classes/LevelMeter.h
+++ src/utils/custom_juce_classes/LevelMeter.h
@@ -21,23 +21,43 @@ public:
     {
         auto bounds = getLocalBounds().toFloat();
 
+        if(orientation == Orientations::Horizontal)
+            bounds.setX(bounds.getX() - 6);
+        else
+            bounds.setY(bounds.getY() + 6);
+            
+
         meterLevel = valueSupplier();
+
+        g.setColour(outlineColour);
+        g.drawRoundedRectangle(bounds, cornerSize, 1.0);
 
         g.setColour(backgroundColour);
         g.fillRoundedRectangle(bounds, cornerSize);
-
+        
         g.setColour(meterColor);
 
         if(orientation == Orientations::Horizontal)
         {
-            const auto scaledX = juce::jmap(meterLevel, -60.0f, 6.0f, 0.0f, static_cast<float>(getWidth()));
+            const auto scaledX = juce::jmap(meterLevel, -90.0f, 0.0f, 0.0f, static_cast<float>(getWidth() - 6));
             g.fillRoundedRectangle(bounds.removeFromLeft(scaledX), cornerSize);
         }
         else
         {
-            const auto scaledY = juce::jmap(meterLevel, -60.0f, 6.0f, 0.0f, static_cast<float>(getHeight()));
+            const auto scaledY = juce::jmap(meterLevel, -90.0f, 0.0f, 0.0f, static_cast<float>(getHeight() - 6));
             g.fillRoundedRectangle(bounds.removeFromBottom(scaledY), cornerSize);
         }
+
+        if(clipped)
+        {   
+            g.setColour(juce::Colours::red);
+
+            if(orientation == Orientations::Horizontal)
+                g.fillRect(bounds.getX() + bounds.getWidth(), bounds.getY(), 6.0, bounds.getHeight());
+            else
+                g.fillRect(bounds.getX(), bounds.getY() - 6.0, bounds.getWidth(), 6.0);
+        }
+
     }
 
     
@@ -51,8 +71,16 @@ public:
         backgroundColour = newColor;
     }
 
+    void setOutlineColour(juce::Colour newColor)
+    {
+        outlineColour = newColor;
+    }
+
     void timerCallback()
     {
+        if (meterLevel >= 0)
+            clipped = true;
+
         repaint();
     }
 
@@ -62,14 +90,29 @@ public:
         Vertical
     };
 
+    void mouseDown (const MouseEvent& event) override
+    {
+        if(orientation == Orientations::Horizontal)
+        {
+            if(event.getPosition().getX() > getBounds().getWidth() - 7)
+                clipped = false;
+        }
+        else
+        {
+            if(event.getPosition().getY() < 7)
+                clipped = false;
+        }
+    }
+
 
 private:
 
     float meterLevel{-30.0f};
     int orientation;
     float cornerSize {1.5};
+    bool clipped {false};
 
-    juce::Colour meterColor{juce::Colours::white}, backgroundColour {juce::Colours::white.withBrightness(0.4f)};
+    juce::Colour meterColor{juce::Colours::white}, backgroundColour {juce::Colours::white.withBrightness(0.4f)}, outlineColour {juce::Colours::black};
 
     std::function<float()> valueSupplier;
 
